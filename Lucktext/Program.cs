c3131a39d767f8cf607665c71836031445c75a36
Elliot Rydell <elliot.rydell@elev.ga.ntig.se>
2022-08-30 10:27:59 +0200

    initial commit

---
+++ Lucktext/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Write your name:");
+string name = Console.ReadLine();
+
+Console.WriteLine("Write a game name:");
+string game = Console.ReadLine();
+
+Console.WriteLine("Write the country you live in:");
+string country = Console.ReadLine();
+
+Console.WriteLine("Write the word family:");
+string family = Console.ReadLine();
+
+Console.WriteLine($"HEY! {name}, you are absolute garbage at {game}.");
+Console.WriteLine($"You are so bad that all of {country} and even your {family} laugh at you!");
+Console.ReadLine();
